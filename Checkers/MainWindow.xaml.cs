127bc136e9e38952c83aa252821932994ada7057
eldarkaaa <112446396+eldarkaaa@users.noreply.github.com>
2022-11-21 21:39:43 +0300

    Implementation of the move

--- Checkers/MainWindow.xaml.cs
+++ Checkers/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace Checkers
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool player1_move = true, player2_move=false;
+        int FieldRow, FieldColumn, WhiteRow, WhiteColumn, BlackRow, BlackColumn;
+        Button white = new Button();
+        Button black = new Button();
+        Button field = new Button();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void Field_Click(object sender, EventArgs e)
+        {
+            field = (Button)sender;
+            FieldRow = Grid.GetRow(field);
+            FieldColumn = Grid.GetColumn(field);
+            if (player1_move && white != null)
+            {
+                if (FieldColumn - WhiteColumn <= 1 && WhiteColumn - FieldColumn <= 1 && WhiteRow - FieldRow == 1)
+                {
+                    Grid.SetColumn(white, FieldColumn);
+                    Grid.SetRow(white, FieldRow);
+                    player1_move = false;
+                    player2_move = true;
+                    white = null;
+                }
+            }
+            else if (player2_move && black != null)
+            {
+                if (FieldColumn - BlackColumn <=1 && BlackColumn - FieldColumn <=1 && FieldRow - BlackRow == 1)
+                {
+                    Grid.SetColumn(black, FieldColumn);
+                    Grid.SetRow(black, FieldRow);
+                    player1_move = true;
+                    player2_move = false;
+                    black = null;
+                }
+            }
+        }
+
+        public void White_Click(object sender, EventArgs e)
+        {
+            white = (Button)sender;
+            WhiteRow = Grid.GetRow(white);
+            WhiteColumn = Grid.GetColumn(white);
+        }
+
+        public void Black_Click(object sender, EventArgs e)
+        {
+            black = (Button)sender;
+            BlackRow = Grid.GetRow(black);
+            BlackColumn = Grid.GetColumn(black);
         }
     }
 }
